fded185e43687f6950db09d40a00154e82c5aecb
hj7321 <oboki2001@naver.com>
2024-04-30 10:06:43 +0900

    문제 푸는 중

--- algorithm/programmers/level1/240430_3.js
+++ algorithm/programmers/level1/240430_3.js
@@ -1,1 +1,11 @@
 // 연습문제 - 최대공약수와 최소공배수
+
+function solution(n, m) {
+  // (최소공배수) = n*m / (최대공약수)
+  let arr = [1, 1];
+  for (let i = 1; i <= Math.min(n, m); i++) {
+    if (n % i === 0 && m % i === 0) arr[0] *= i;
+  }
+  arr[1] = (n * m) / arr[0];
+  return arr;
+}
