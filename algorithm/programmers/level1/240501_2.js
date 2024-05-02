c16c74fedca54603903036b25ed9c96f210060c2
hj7321 <oboki2001@naver.com>
2024-05-02 09:53:57 +0900

    코딩테스트 문제 푼 것 정리

--- algorithm/programmers/level1/240501_2.js
+++ algorithm/programmers/level1/240501_2.js
@@ -1,1 +1,20 @@
-// 연습문제 - 
+// 연습문제 - 삼총사
+
+// 첫 번째, 두 번째 선택한 숫자를 더하고 그것과 더했을 때 0이 되는 세 번째 숫자를 고르는 방법?
+
+function solution(number) {
+  let answer = 0;
+  let n = number.length;
+  for(let i = 0; i < n; i++) {
+    for(let j = i+1; j < n; j++) {
+      let sum = number[i] + number[j];
+      for(let k = j+1; k < n; k++) {
+        if(sum + number[k]) answer++;
+      }
+    }
+  }
+  return answer;
+}
+
+let result = solution([-2, 3, 0, 2, 5]);
+console.log(result);
